ad549bb0f07491b1fe3926a713912bae0c15e43f
scoavoux <samuel.coavoux@gmail.com>
2015-05-31 12:08:17 +0200

    basic function to insert crossreferences in pandoc-crossref format #16

---
+++ PandocCrossrefCite.py
@@ -0,0 +1,47 @@
+# -*- coding: utf-8 -*-
+import sublime
+import sublime_plugin
+import re
+
+
+def get_labels(view):
+    # trouver l'ensemble des références à un tableau ou une figure dans view
+    # regex pour les labels
+    tbl_reg = re.compile("^\w*:(.*?){#(tbl):([a-zA-Z0-9-_]*)?}")
+    fig_reg = re.compile("^.*\!\[(.*?)\]\(.*?\)\s*{#(fig):([a-zA-Z0-9-_]*)?}")
+    # récupérer l'ensemble du texte
+    content = view.split_by_newlines(sublime.Region(0, view.size()))
+    labels = []
+    for elmt in content:
+        line = view.substr(elmt)
+        match = tbl_reg.match(line)
+        if not match:
+            match = fig_reg.match(line)
+        if match:
+            caption = match.group(1)
+            genre = match.group(2)
+            label = match.group(3)
+            labels.append({
+                "caption": caption.strip(),
+                "genre": genre,
+                "label": label
+                })
+    return(labels)
+
+
+class PandocCrossrefCiteCommand(sublime_plugin.TextCommand):
+    def run(self, edit):
+        view = self.view
+        point = view.sel()[0].b
+        completions = get_labels(view)
+
+        def on_done(i):
+            if i < 0:
+                return
+
+            cite = "[@" + completions[i]["genre"] + ":" + completions[i]["label"] + "]"
+            view.run_command("insert_cite",{"a": point, "b": point, "cite": cite})
+
+
+        completion_strings = [entry['genre'] + " : " + entry['caption'] + " (" + entry['label'] + ")" for entry in completions]
+        view.window().show_quick_panel(completion_strings, on_done)
