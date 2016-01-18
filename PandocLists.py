dfa84ec76a29f38309d978e75a2f4a2d45386d25
Sarah Beniamine <sbeniamine@linguist.univ-paris-diderot.fr>
2016-01-18 23:46:09 +0100

    New support for lists, including numeric bullet points continuation, better regexes.

--- PandocLists.py
+++ PandocLists.py
@@ -10,7 +10,7 @@ class PandocListsCommand(sublime_plugin.TextCommand):
     point = view.sel()[0].b
     line = view.substr(sublime.Region(view.line(point).a, point))
     bullets_trigger = re.compile("^( *)([*-+]|#\.) .+")
-    numeric_list_trigger = re.compile("^( *)(\d+)([.)]) .+")
+    numeric_list_trigger = re.compile("^( *)([(])?(\d+)([.)]) .+")
 
     bullet = bullets_trigger.match(line)
     numeric = numeric_list_trigger.match(line)
@@ -21,6 +21,7 @@ class PandocListsCommand(sublime_plugin.TextCommand):
         new_bullet = bullet.groups()[1]
     elif numeric:
         spaces = numeric.groups()[0]
-        new_bullet = str(int(numeric.groups()[1])+1)+numeric.groups()[2]
+        before = numeric.groups()[1] or ""
+        new_bullet = before+str(int(numeric.groups()[2])+1)+numeric.groups()[3]
 
     self.view.insert(edit, point, "\n"+spaces+new_bullet+" ")
