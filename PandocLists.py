93acef36ca105bdde9bdf98bb2c71df0f5ec2f27
Sarah Beniamine <sbeniamine@linguist.univ-paris-diderot.fr>
2016-04-10 19:04:46 -0700

    Fixes indentation.

--- PandocLists.py
+++ PandocLists.py
@@ -5,23 +5,23 @@ import re
 
 
 class PandocListsCommand(sublime_plugin.TextCommand):
-  def run(self, edit):
-    view = self.view
-    point = view.sel()[0].b
-    line = view.substr(sublime.Region(view.line(point).a, point))
-    bullets_trigger = re.compile("^( *)([*-+]|#\.) .+")
-    numeric_list_trigger = re.compile("^( *)([(])?(\d+)([.)]) .+")
+    def run(self, edit):
+        view = self.view
+        point = view.sel()[0].b
+        line = view.substr(sublime.Region(view.line(point).a, point))
+        bullets_trigger = re.compile("^( *)([*-+]|#\.) .+")
+        numeric_list_trigger = re.compile("^( *)([(])?(\d+)([.)]) .+")
 
-    bullet = bullets_trigger.match(line)
-    numeric = numeric_list_trigger.match(line)
-    spaces = ""
+        bullet = bullets_trigger.match(line)
+        numeric = numeric_list_trigger.match(line)
+        spaces = ""
 
-    if bullet:
-        spaces = bullet.groups()[0]
-        new_bullet = bullet.groups()[1]
-    elif numeric:
-        spaces = numeric.groups()[0]
-        before = numeric.groups()[1] or ""
-        new_bullet = before+str(int(numeric.groups()[2])+1)+numeric.groups()[3]
+        if bullet:
+            spaces = bullet.groups()[0]
+            new_bullet = bullet.groups()[1]
+        elif numeric:
+            spaces = numeric.groups()[0]
+            before = numeric.groups()[1] or ""
+            new_bullet = before+str(int(numeric.groups()[2])+1)+numeric.groups()[3]
 
-    self.view.insert(edit, point, "\n"+spaces+new_bullet+" ")
+        self.view.insert(edit, point, "\n"+spaces+new_bullet+" ")
